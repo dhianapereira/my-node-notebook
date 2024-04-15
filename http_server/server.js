989bb25e1efa61eb3b3ba9ae630811b28020961f
dhianapereira <dhianaslv@outlook.com>
2024-04-15 19:48:36 -0300

    feat: Create a simple CRUD

--- http_server/server.js
+++ http_server/server.js
@@ -1,9 +1,43 @@
-import {createServer} from 'node:http'
+import {fastify} from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
-const server = createServer((request, response)=>{
-    response.write('Hello developer')
+const server = fastify()
 
-    return response.end()
+const database = new DatabaseMemory()
+
+server.get('/videos', (request)=>{
+    const search = request.query.search
+    const videos = database.list(search)
+    return videos
 })
 
-server.listen(3333)
+server.post('/videos', (request, response)=>{
+    const {title, description, duration} = request.body
+    database.create({
+        title,
+        description,
+        duration,
+    })
+    return response.status(201).send()
+})
+
+server.put('/videos/:id', (request, response)=>{
+    const id = request.params.id
+    const {title, description, duration} = request.body
+    database.update(id, {
+        title,
+        description,
+        duration,
+    })
+    return response.status(204).send()
+})
+
+server.delete('/videos/:id', (request, response)=>{
+    const id = request.params.id
+    database.delete(id)
+    return response.status(204).send()
+})
+
+server.listen({
+    port: 3333,
+})
