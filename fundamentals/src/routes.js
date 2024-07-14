29b4f3d3b3268fe0c0b1cafebcecc9000fd7e8a5
dhiana <dhianaslv@outlook.com>
2024-07-14 00:17:41 -0300

    Fundamentals of nodeJS

---
+++ fundamentals/src/routes.js
@@ -0,0 +1,72 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      try {
+        database.update('tasks', id, {
+          title,
+          description,
+        })
+
+        return res.writeHead(204).end()
+      } catch (error) {
+        return res.writeHead(404).end(error.message)
+      }
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      try {
+        database.delete('tasks', id)
+
+        return res.writeHead(204).end()
+      } catch (error) {
+        return res.writeHead(404).end(error.message)
+      }
+    }
+  }
+]
